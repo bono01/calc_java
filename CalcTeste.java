37fd0cdbe561b687aea0fb6e694ae5aa35be9093
Victor Bono <victormb1914@gmail.com>
2024-03-26 21:29:29 -0300

    add 2

--- CalcTeste.java
+++ CalcTeste.java
@@ -7,7 +7,7 @@ public class CalcTeste {
                 Double.parseDouble(JOptionPane.showInputDialog("Digite seu número preferido")));
         double resultadoSoma = calc.somar(2, 2);
         System.out.println(resultadoSoma);
-
+        System.out.print(resultadoSoma);
         System.out.println(calc.somarComPreferido(
                 Double.parseDouble(JOptionPane.showInputDialog("Digite seu número para somar com o preferido"))));
 
