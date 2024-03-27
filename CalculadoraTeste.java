ebc6b81010a1eb490ef497a403c51c594dc9892b
Victor Bono <victormb1914@gmail.com>
2024-03-26 21:32:30 -0300

    ade3

--- CalculadoraTeste.java
+++ CalculadoraTeste.java
@@ -1,13 +1,13 @@
-import javax.swing.JOptionPane;
-
 public class CalculadoraTeste {
     public static void main(String[] args) {
         Calculadora calc = new Calculadora(9);
         //testar soma
         double soma = calc.somar(2, 3);
         System.out.println(soma);
+        System.out.print(soma);
+    
         //testar subtracao
         double sub = calc.subtrair(2, 3);
-        System.out.println(sub);
+        System.out.println(sub); 
     }
 }
